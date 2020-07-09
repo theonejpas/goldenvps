c9e2fa3217e443225d0f468036ef97602b427125
theonejpas <40813121+theonejpas@users.noreply.github.com>
2020-07-08 23:47:40 -0500

    update goldenvps.sh

--- goldenvps.sh
+++ goldenvps.sh
@@ -140,7 +140,7 @@ mv -f ${SCPinstal}/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
 fun_ip
-wget -O /usr/bin/trans https://raw.githubusercontent.com/GoldenADMPRO/GOLDEN-ADM-MANAGER/master/Install/trans &> /dev/null
+wget -O /usr/bin/trans https://raw.githubusercontent.com/theonejpas/goldenvps/master/Install/trans &> /dev/null
 msg -bar2
 msg -ama "[ GOLDEN ADM PRO SCRIPT]"
 [[ $1 = "" ]] && funcao_idioma || {
